e0ce50111317d5c7a03014cb09177fc8caf453ed
FabioCalixto <fabiocalixto16@gmail.com>
2021-03-17 09:46:08 +0100

    Middlewares

--- app.js
+++ app.js
@@ -5,15 +5,43 @@ app.use(express.json());
 
 const contactos = ['Fabio','Mario','Vilar', 'Calixto'] ;
 
+/*
+app.use( (req, res, next)=> {
+   
+      console.log("Acessou o Middleweres!");
+      next();
+
+});*/
+
+function valContato(req, res, next){
+      if(!req.body.nome){
+          return res.status(400).json({
+              error: "Necessário enviar o Nome!"
+          })
+      }
+      return next();
+}
+
+function valPosContato(req,res, next){
+      if(!contactos[req.params.id]){
+          return res.status(400).json({
+              error: "Contacto não Encontrado"
+          })
+      }
+      return next();
+}
+
+
 //USANDO O GET NA REST API PARA LISTAR
 app.get("/contato", (req, res) => {
- return res.json(contactos);
+    
+    return res.json(contactos);
 
 });
 
 
 
-app.get("/contato/:id", (req, res)=> {
+app.get("/contato/:id", valPosContato, (req, res)=> {
     
     const {id} = req.params;
    
@@ -28,7 +56,7 @@ app.get("/contato/:id", (req, res)=> {
 
 //USANDO O POST PARA CADASTRAR
 
-app.post("/contato", (req, res)=>{
+app.post("/contato", valContato, (req, res)=>{
     const {nome} = req.body;
     contactos.push(nome);
     return res.json(contactos);
@@ -36,7 +64,7 @@ app.post("/contato", (req, res)=>{
 });
 
 //USANDO O PUT NA REST PARA EDITAR
-app.put("/contato/:id", (req, res)=> {
+app.put("/contato/:id",valContato,valPosContato , (req, res)=> {
     const {id} = req.params;
     const {nome} = req.body;
 
@@ -46,7 +74,7 @@ app.put("/contato/:id", (req, res)=> {
 });
 
 //USANDO O DELETE NA REST API PARA APAGAR UM REGISTRO
-app.delete("/contato/:id", (req, res) =>{
+app.delete("/contato/:id",valPosContato , (req, res) =>{
         const  {id } = req.params;
         contactos.splice(id,1);
         return res.json(contactos);
